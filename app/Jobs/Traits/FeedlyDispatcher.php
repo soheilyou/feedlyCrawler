a4977b962a4ba302fe863548aa74aed166a88c63
soheilyou <myparstik@gmail.com>
2022-09-02 22:13:27 +0430

    remove extra code from FeedlyDispatcher

--- app/Jobs/Traits/FeedlyDispatcher.php
+++ app/Jobs/Traits/FeedlyDispatcher.php
@@ -2,15 +2,12 @@
 
 namespace App\Jobs\Traits;
 
-use App\Classes\Organize\NewItemsProcess;
-
 trait FeedlyDispatcher
 {
     public static string $feedlyQueue = "feedly";
 
     public static function dispatchBasedOnConfig(...$args)
     {
-        $feedId = $args[0];
         /**
          * check config first
          */
