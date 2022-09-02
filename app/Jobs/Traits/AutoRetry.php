b58d8cd4a80cd25e5d08a32dfdd8bfc450ffeb93
soheilyou <myparstik@gmail.com>
2022-09-02 13:49:52 +0430

    add AutoRetry trait to autoRetry failed jobs

---
+++ app/Jobs/Traits/AutoRetry.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs\Traits;
+
+use DateTime;
+use Illuminate\Queue\Middleware\ThrottlesExceptions;
+
+trait AutoRetry
+{
+    protected int $maxAttempts = 3;
+    protected int $decayMinutes = 10;
+    protected int $retryUntil = 24; // Hours
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array
+     */
+    public function middleware(): array
+    {
+        return [new ThrottlesExceptions($this->maxAttempts, $this->decayMinutes)];
+    }
+
+    /**
+     * Determine the time at which the job should timeout.
+     *
+     * @return DateTime
+     */
+    public function retryUntil(): DateTime
+    {
+        return now()->addHours($this->retryUntil);
+    }
+}
